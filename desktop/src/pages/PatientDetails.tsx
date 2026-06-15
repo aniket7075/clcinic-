e64ce5968e56d078ce56bb947c5025187c0f4346
Aniket Husukale <husukaleaniket@gmail.com>
2026-06-15 17:33:18 +0530

    Fix typescript unused import useNavigate

--- desktop/src/pages/PatientDetails.tsx
+++ desktop/src/pages/PatientDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../store';
 import api from '../api/axios';
